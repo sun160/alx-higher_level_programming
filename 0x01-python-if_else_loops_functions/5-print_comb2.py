87d7e0729f75ac0fb5c6bfaffd6f39a8ab3abb54
sun160 <you@example.com>
2023-10-31 20:24:21 -0700

    numbers in python1

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for n in range(100):
+    print("{:2d}".format(n), end="\n" if n == 99 else ", ")
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for t in range(10):
+    for w in range(t, 10):
+        if t < w:
+            print("{:d},{:d}".format(t, w), 
+                    end="\n" if t == 8 and w == 9 else ", ")
