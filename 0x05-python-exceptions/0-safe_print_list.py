2df50a4a990bb251eb04164003bf98bb756d9bd3
sun160 <n.abdelmonem@yahoo.com>
2023-11-20 10:08:34 -0800

    exceptions1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,5 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[k], end="")
             m += 1
         except IndexError:
-            break
-    print("")
-    return
+            None
+    print()
