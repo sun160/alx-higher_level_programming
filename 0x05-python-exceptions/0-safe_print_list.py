0a427dc3f0aecfab25fa5dc280e2d5a15f44fe9e
sun160 <n.abdelmonem@yahoo.com>
2023-11-20 10:01:17 -0800

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     m = 0
-    for m in range(x):
+    while m is not (x):
         try:
             print(my_list[i], end="")
             m += 1
